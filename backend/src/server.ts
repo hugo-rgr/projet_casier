9ebca1d200c8b7c2abe451d4afb96c9d17e6694f
hugo-rgr <hugo@silk-digital.com>
2025-04-25 15:26:34 +0200

    init user model and routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,19 +2,23 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
+import userRoutes from './routes/users';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
 dotenv.config();
 connectDB();
+
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('API fonctionnelle');
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
