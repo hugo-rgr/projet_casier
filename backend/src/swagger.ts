d8d1b244000afa0459f1216ccb795298cea8ac56
hugo-rgr <hugo@silk-digital.com>
2025-06-20 01:26:02 +0200

    Implement Swagger change server port

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJSDoc.Options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'http://localhost:${process.env.PORT || 3000}',
             },
         ],
         components: {
